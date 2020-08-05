6cf1cebe3ce7a934018d1b69172a55dc54d4e631
Quinn94 <mayquinn94@gmail.com>
2020-08-05 08:42:39 +0800

    static folder

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const path = require("path");
+const members = require("./Members");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+const logger = (req, res, next) => {
+  console.log("Hello");
+  next();
+};
+
+//Init middleware
+app.use(logger);
+
+//Get all members
+app.get("/api/members", (req, res) => res.json(members));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000; //look at the environment of the user first
 
