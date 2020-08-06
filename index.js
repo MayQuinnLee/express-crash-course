bc6d50dd763ff74642755a011d82a2c6f0b3e395
Quinn94 <mayquinn94@gmail.com>
2020-08-06 08:54:31 +0800

    api res

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const path = require("path");
+const logger = require("./middleware/logger");
 const members = require("./Members");
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log("Hello");
-  next();
-};
-
 //Init middleware
-app.use(logger);
+// app.use(logger);
 
 //Get all members
 app.get("/api/members", (req, res) => res.json(members));
+
+//Get single member
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
